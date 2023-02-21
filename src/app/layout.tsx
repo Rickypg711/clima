148d417bf246bd40ef687cf2ffc2200b2ad53ce1
Rickypg711 <77697094+Rickypg711@users.noreply.github.com>
2023-02-21 15:45:04 -0800

    clima

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className='bg-gradient-to-b from-blue-400 to-blue-900  h-screen'>{children}</body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,89 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+"use client";
+import { useState, useEffect } from "react";
+import { IoMdPartlySunny } from "react-icons/io";
+import axios from "axios";
 
 export default function Home() {
+
+
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const [backgroundImage, setBackgroundImage] = useState("");
+
+  const apiKey = `87e455a72ede06909556a35b713e8890`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${apiKey}`;
+
+  const searchLocation = (event: any) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+        setLocation("");
+      });
+    }
+  };
+
+  useEffect(() => {
+    const unsplashAccessKey = `M0OH7D48h871fb0jVqbFy8qycRDrD6xC7zSSx6rObDY`;
+    const unsplashUrl = `https://api.unsplash.com/search/photos?page=1&query=${location}&client_id=${unsplashAccessKey}`;
+    
+    
+    axios.get(unsplashUrl).then((response) => {
+      setBackgroundImage(response.data.results[0].urls.regular);
+    });
+  
+  }, [location]);
+
+  const backgroundStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+    <div   style={backgroundStyle} className=' h-screen'>
+      <main >
+        <div className="top w-full">
+          <div className="title text-center items-center py-7">
+            <h1 className="text-7xl text-orange-600">ClimApp</h1>
+          </div>
+          <div className="text-center p-2">
+            <input
+              id="input"
+              value={location}
+              onChange={(event) => setLocation(event.target.value)}
+              placeholder="Enter Location"
+              onKeyUp={searchLocation}
+              className="px-4 py-2 border border-orange-400 rounded-xl focus:outline-none focus:ring focus:border-blue-300 text-center"
+              type="text"
             />
-          </a>
+          </div>
+          <div className=" items-center text-center">
+            <p className="text-4xl">{data.name}</p>
+
+            {data.main ? <p>{data.main.temp}</p> : null}
+          </div>
+
+          <div className="description ">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
         </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+        <div className="bottom flex justify-evenly text-center  mx-4 my-auto p-4 border-3 bg-slate-50/50 rounded-3xl">
+          <div className="feelsLike">
+            <h2>Feels Like</h2>
+            {data.main ? <p>{data.main.feels_like}&deg;F</p> : null}
+          </div>
+          <div className="humidity">
+            <h2>Humidity</h2>
+            {data.main ? <p>{data.main.humidity}%</p> : null}
+          </div>
+          <div className="wind">
+            <h2>Wind Speed</h2>
+            {data.main ? <p>{data.wind.speed}&deg;F</p> : null}
+          </div>
         </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </main>
+    </div>
+  );
 }
